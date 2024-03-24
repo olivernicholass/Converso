30a6bd525177089668141a78ed22b5f3797a5d27
volumeOG <stefanspataro@hotmail.com>
2024-03-23 17:50:13 -0700

    pulling changes

--- html/thread.php
+++ html/thread.php
@@ -3,5 +3,19 @@ require 'connect.php';
 
 //Get Thread URL - for now, set to 0 for sample
 //$thread_id = isset($_GET['thread_id']) ? $_GET['thread_id'] : null;
-$threadid = 0;
+$threadid = 7;
 
+$sql_thread = "SELECT * FROM thread WHERE threadid = ?";
+$prep_stmt = mysqli_prepare($connection, $sql_thread);
+mysqli_stmt_bind_param($prep_stmt, "i", $threadid);
+mysqli_stmt_execute($prep_stmt);
+$result_thread = mysqli_stmt_get_result($prep_stmt);
+$thread = mysqli_fetch_assoc($result_thread);
+
+
+if($thread){
+ // Output thread details
+    echo "<h1>{$thread['title']}</h1>";
+    echo "<p>{$thread['content']}</p>";
+
+}
