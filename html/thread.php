ecafe9d1feb2a25c4f677a8585c8ced6be1da18e
olivernicholass <omedgyesi@icloud.com>
2024-03-24 11:23:00 -0700

    Adjusted Threads to display comments properly + Remove unnecessary CSS

--- html/thread.php
+++ html/thread.php
@@ -11,66 +11,94 @@
 </head>
 
 <body>
-<div class="container">
-    <?php
-    require 'connect.php';
+    <div class="container">
+        <?php
+        require 'connect.php';
 
-    // Dynamically display correct thread based on threadId
-    // We get our thread.php?thread_id="Unique Thread ID" 
-    
-    $threadId = isset($_GET['thread_id']) ? $_GET['thread_id'] : null;
+        $threadId = isset($_GET['thread_id']) ? $_GET['thread_id'] : null;
 
-    $sql = "SELECT thread.*, section.sname, thread.likes 
+        // Dynamically display correct thread based on threadId
+        // We get our thread.php?thread_id="Unique Thread ID" 
+
+        $sql = "SELECT thread.*, section.sname, thread.likes 
                     FROM thread 
                     INNER JOIN section ON thread.sectionid = section.sectionid
                     WHERE thread.threadid = ?";
                     
-    $preparedStmt = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($preparedStmt, "i", $threadId);
-    mysqli_stmt_execute($preparedStmt);
-    $result = mysqli_stmt_get_result($preparedStmt);
-    $thread = mysqli_fetch_assoc($result);
+        $preparedStmt = mysqli_prepare($connection, $sql);
+        mysqli_stmt_bind_param($preparedStmt, "i", $threadId);
+        mysqli_stmt_execute($preparedStmt);
+        $result = mysqli_stmt_get_result($preparedStmt);
+        $thread = mysqli_fetch_assoc($result);
 
-    if ($thread) {
-        ?>
-        <div class="card mb-4 main-post-card">
-            <div class="user-info">
-                <img src="../images/user2.png" class="user-profile">
-                <p>
-                    u/<?php echo "USERNAME VARIABLE GOES HERE"; ?>
-                </p>
-            </div>
-            <div class="card-body">
-                <h5 class="cr-light" style="font-size: 12px;">c/<?php echo $thread['sname']; ?></h5>
-                <h5 class="card-title"><?php echo $thread['title']; ?></h5>
-                <p class="card-text"><?php echo $thread['content']; ?></p>
-            </div>
-            <img src="<?php echo $thread['threadImage']; ?>" class="card-img-top" alt="Image Description">
+        if ($thread) {
+            ?>
+            <div class="card mb-4 main-post-card">
+                <div class="user-info">
+                    <img src="../images/user2.png" class="user-profile">
+                    <p>
+                        u/<?php echo "USERNAME VARIABLE GOES HERE"; ?>
+                    </p>
+                </div>
+                <div class="card-body">
+                    <h5 class="cr-light" style="font-size: 12px;">c/<?php echo $thread['sname']; ?></h5>
+                    <h5 class="card-title"><?php echo $thread['title']; ?></h5>
+                    <p class="card-text"><?php echo $thread['content']; ?></p>
+                </div>
+                <img src="<?php echo $thread['threadImage']; ?>" class="card-img-top" alt="Image Description">
 
-            <div class="post-buttons">
-                <button class="like-button" onclick="likeThread(<?php echo $thread['threadid']; ?>)">
-                    <img src="../icons/like.png" alt="Like Icon"> Like
-                </button>
-                <button class="comment-button">
-                    <img src="../icons/comment.png" alt="Comment Icon"> Comment
-                </button>
-                <button class="share-button">
-                    <img src="../icons/share.png" alt="Share Icon"> Share
-                </button>
+                <div class="post-buttons">
+                    <button class="like-button" onclick="likeThread(<?php echo $thread['threadid']; ?>)">
+                        <img src="../icons/like.png" alt="Like Icon"> Like
+                    </button>
+                    <button class="comment-button">
+                        <img src="../icons/comment.png" alt="Comment Icon"> Comment
+                    </button>
+                    <button class="share-button">
+                        <img src="../icons/share.png" alt="Share Icon"> Share
+                    </button>
+                </div>
             </div>
-
-            <div class="comment-container">
+            
+            <div class="leave-comment">
                 <textarea rows="1" type="text" class="comment-text" placeholder="Leave a Comment"></textarea>
-                <button type="submit" class="btn btn-primary">Post</button>
+                <button type="submit" class="btn btn-primary post">Post</button>
             </div>
-        </div>
-    <?php
-    } else {
-        echo "<p>No thread found.</p>";
-    }
-    ?>
-</div>
+            
+            <div class="comment-container">
+                <?php
+                $comments = array(
+                    array(
+                        'username' => 'User1',
+                        'date' => '2024-03-24',
+                        'content' => 'Comment 1'
+                    ),
+                    array(
+                        'username' => 'User2',
+                        'date' => '2024-03-25',
+                        'content' => 'Comment 2'
+                    ),
+                );
 
+                foreach ($comments as $comment) {
+                    ?>
+                    <div class="comment">
+                        <div class="comment-info">
+                            <div class="username"><?php echo $comment['username']; ?></div>
+                            <div class="date"><?php echo $comment['date']; ?></div>
+                        </div>
+                        <div class="comment-content"><?php echo $comment['content']; ?></div>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+        <?php
+        } else {
+            echo "<p>No thread found.</p>";
+        }
+        ?>
+    </div>
 </body>
 
 </html>
