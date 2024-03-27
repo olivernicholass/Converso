1266680875cda8c31f0aa05ea70d33e8d3b33c1f
volumeOG <stefanspataro@hotmail.com>
2024-03-27 11:45:49 -0700

    working on connect.php

--- html/connect.php
+++ html/connect.php
@@ -1,5 +1,7 @@
 <?php
-$connection = mysqli_connect("localhost", "56862295", '56862295', "db_56862295", 3306);
+//LOCAL USER/PASS:
+//SERVER USER/PASS: 56862295/56862295
+$connection = mysqli_connect("localhost", "ss217", '$$Madstef1', "db_56862295", 3306);
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
-  }
+}
