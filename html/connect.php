ef0c01481fc177a0017cc68176d078c5c0ee2f0e
volumeOG <stefanspataro@hotmail.com>
2024-03-28 15:25:22 -0700

    changed connection

--- html/connect.php
+++ html/connect.php
@@ -4,10 +4,10 @@
 //SERVER SOURCE/USER/PASS/DATABASE/PORT: localhost/56862295/56862295/db_56862295/3307
 
 //for local
-$connection = mysqli_connect("localhost", "root", '', "fanpit", 3307);
+//$connection = mysqli_connect("localhost", "root", '', "fanpit", 3307);
 
 //for server
-//$connection = mysqli_connect("localhost", "56862295", '56862295', "db_56862295", 3306);
+$connection = mysqli_connect("localhost", "56862295", '56862295', "db_56862295", 3306);
 
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
